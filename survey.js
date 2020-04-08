0b424ff8d8cebc81bd58d0464282e0c3918399f0
Madeline Guman <mads890@yahoo.com>
2020-04-08 12:47:00 -0400

    fixed survey refresh

--- survey.js
+++ survey.js
@@ -1,23 +1,12 @@
 'use strict';
 
 function formSubmit() {
-    $('.feedback-form').on('click', 'button', event => {
+    $('.feedback-form').on('click', '#send', event => {
         event.preventDefault();
-        $('feedback-form').reset();
         $('.feedback-form-container').addClass('hidden');
         $('.results-container').removeClass('hidden');
         console.log('formSubmit ran')
     })
 }
 
-function retakeSurvey() {
-    $('.results-container').submit(event => {
-        event.preventDefault();
-        $('.results-container').addClass('hidden');
-        $('.feedback-form-container').removeClass('hidden');
-        console.log('retakeSurvey ran')
-    })
-}
-
 $(formSubmit());
-$(retakeSurvey());
